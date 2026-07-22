d738377260238db6d75a60768eb0de4ec4d74730
JayashreeNayak-coder <jayashreefour20@gmail.com>
2026-07-22 09:26:50 +0530

    Time: 4 ms (82.37%), Space: 44.1 MB (95.29%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length()!=t.length())return false;
+
+        int hash[]=new int[26];
+
+        for(int i=0;i<s.length();i++){
+            hash[(int)s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            hash[(int)t.charAt(i)-'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(hash[i]!=0)return false;
+        }
+
+
+        return true;
+    }
+}
